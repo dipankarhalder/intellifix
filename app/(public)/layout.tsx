65177f6ca09e232f8b34fa51d45e829f389dc26d
Dipankar Halder <dipankar@Dipankars-MacBook-Pro.local>
2024-06-21 03:23:32 +0530

    login screen

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -6,7 +6,7 @@ export default function PublicLayout({ children }: Readonly<{
       <div className="flex h-screen w-1/2 items-center justify-center p-8">
         {children}
       </div>
-      <div className="w-1/2 h-screen bg-gray-100 border-r bg-gradient-to-r from-sky-600 to-indigo-800">
+      <div className="w-1/2 h-screen bg-gray-100 bg-gradient-to-r from-sky-600 to-indigo-800">
       </div>
     </main>
   );
